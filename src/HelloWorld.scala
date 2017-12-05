84124902224fac8be46d1e19a37b36be32d89882
aleskandro <aleskandro@lucylaika.ovh>
2017-12-05 12:59:33 +0100

    Starting handling for POST and JSON
    - Created RequestHandler class
    - Imported spray-json
    - Downgraded scala sdk to 2.11 to get deploy working with spray-json (still no releases available on default maven repository for 2.12 scala sdk)

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,11 +1,17 @@
+package org.unict.ing.advlanguages.boxoffice
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
-
+import akka.util.Timeout
+import akka.pattern.ask
+import scala.concurrent.duration._
 import scala.io.StdIn
-
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
 object HelloWorld {
   val host = "localhost"
   val port = 8080
@@ -14,13 +20,36 @@ object HelloWorld {
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
     implicit val executionContext = system.dispatcher
-    val route =
+    implicit val timeout = Timeout(20.seconds)
+
+    // This is to get (un)marshalling in POST requests, see: https://doc.akka.io/docs/akka-http/current/scala/http/introduction.html#routing-dsl-for-http-servers
+    implicit val healthFormat = jsonFormat2(Health)
+
+    val requestHandler = system.actorOf(RequestHandler.props(), "requestHandler")
+    val route : Route = {
+
       path("hello") {
-        get{
-          complete(StatusCodes.OK, "Hello World!")
+        get {
+          onSuccess(requestHandler ? GetHealthRequest) {
+            case response: HealthResponse =>
+              complete(StatusCodes.OK, s"Everything is ${response.health.status}!")
+            case _ =>
+              complete(StatusCodes.InternalServerError)
+          }
+        } ~
+        post {
+          entity(as[Health]) {
+            statusReport =>
+              onSuccess(requestHandler ? SetStatusRequest(statusReport)) {
+                case response: HealthResponse =>
+                  complete(StatusCodes.OK, s"Posted health as ${response.health.status}: ${response.health.description}")
+                case _ =>
+                  complete(StatusCodes.InternalServerError)
+              }
+          }
         }
       }
-
+    }
 
     val bindingFuture = Http().bindAndHandle(route, host, port)
     println("API on...")
---
+++ src/RequestHandler.scala
@@ -0,0 +1,30 @@
+package org.unict.ing.advlanguages.boxoffice
+
+import akka.actor.{Actor, ActorLogging, Props}
+
+object RequestHandler {
+  def props(): Props = {
+    Props(classOf[RequestHandler])
+  }
+}
+
+case class Health(status: String, description: String)
+case object GetHealthRequest
+case class SetStatusRequest(health: Health)
+case class HealthResponse(health: Health)
+
+class RequestHandler extends Actor with ActorLogging{
+
+  var status: Health = Health("Healthy","Initialized")
+
+  def receive: Receive = {
+
+    case GetHealthRequest =>
+      log.debug("Received GetHealthRequest")
+      sender() ! HealthResponse(status)
+    case request: SetStatusRequest =>
+      log.debug("Updating status to {}", request.health)
+      status = request.health
+      sender() ! HealthResponse(status)
+  }
+}
