830dd2e20e4106e4aad70bbbf0b30f227997602c
aleskandro <aleskandro@lucylaika.ovh>
2017-12-21 19:25:18 +0100

    Custom case classes with codec for Mongo Driver

--- src/resources/ProvaDB.scala
+++ src/resources/ProvaDB.scala
@@ -1,13 +1,15 @@
 package resources
 
 
+import org.bson.codecs.configuration.CodecRegistries
 import org.bson.types.ObjectId
-import org.mongodb.scala.{Completed,
-  MongoClient, MongoCollection,
-  MongoDatabase, Observable,
-  Observer}
-
+import org.mongodb.scala.{Completed, MongoClient, MongoCollection, MongoDatabase, Observable, Observer}
 import org.mongodb.scala.bson._
+
+// Custom case classes codecs for mongo scala driver
+// https://github.com/mongodb/mongo-scala-driver/blob/master/examples/src/test/scala/tour/QuickTourCaseClass.scala
+import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
 
 // case class and companion object
 object UserProva {
@@ -25,14 +27,38 @@ case class UserProva(
 object ProvaDB extends App {
   println("Start")
   // default connection on
-  val mongoClient: MongoClient = MongoClient()
-  val database: MongoDatabase = mongoClient.getDatabase("mydb")
-  val collection: MongoCollection[Document] = database.getCollection("test")
-  val es2: Document = Document("Lorem" -> "Ipsum", "Dolor" -> "Sit", "Amet" -> "consectetuer")
 
-  val insertObservable: Observable[Completed] = collection.insertOne(es2)
+  // Create a codec for the UserProva case class
+
+  // Custom case classes: https://github.com/mongodb/mongo-scala-driver/blob/master/examples/src/test/scala/tour/QuickTourCaseClass.scala
+  val codecRegistry = CodecRegistries.fromRegistries(CodecRegistries.fromProviders(classOf[UserProva]), DEFAULT_CODEC_REGISTRY)
+  val mongoClient: MongoClient = MongoClient()
+  val database: MongoDatabase = mongoClient.getDatabase("mydb").withCodecRegistry(codecRegistry)
+  val collection: MongoCollection[Document] = database.getCollection("test")
+  val es: Document = Document("Lorem" -> "Ipsum", "Dolor" -> "Sit", "Amet" -> "consectetuer")
+
+  val insertObservable: Observable[Completed] = collection.insertOne(es)
+  collection.find().collect().subscribe((results: Seq[Document]) => {
+    println(s"Found: ${results}")
+  })
 
   insertObservable.subscribe(new Observer[Completed] {
+    override def onNext(result: Completed): Unit = println(s"onNext: $result")
+    override def onError(e: Throwable): Unit = println(s"onError: $e")
+    override def onComplete(): Unit = println("onComplete")
+  })
+
+
+  // Custom case classes: https://github.com/mongodb/mongo-scala-driver/blob/master/examples/src/test/scala/tour/QuickTourCaseClass.scala
+  val collection2: MongoCollection[UserProva] = database.getCollection("test2")
+  val es2: UserProva = UserProva("asd", "asdasd", "asda@asdasd")
+
+  val insertObservable2: Observable[Completed] = collection2.insertOne(es2)
+  collection2.find().collect().subscribe((results: Seq[UserProva]) => {
+    println(s"Found: ${results}")
+  })
+
+  insertObservable2.subscribe(new Observer[Completed] {
     override def onNext(result: Completed): Unit = println(s"onNext: $result")
     override def onError(e: Throwable): Unit = println(s"onError: $e")
     override def onComplete(): Unit = println("onComplete")
