9fadb79cef97dfd54656fe3a7769d085c9b5e9ae
Aleskandro <aleskandro@hexaflake.zion.lan>
2017-12-24 09:52:10 +0100

    Seeds for users

--- src/devTests/MongoSeeds.scala
+++ src/devTests/MongoSeeds.scala
@@ -1,7 +1,7 @@
 package devTests
 
 import controllers._
-import model.{Event, EventCreator}
+import model.{Event, EventCreator, User, UserCreator}
 
 import scala.collection.mutable.ListBuffer
 import akka.http.scaladsl.model.DateTime
@@ -16,18 +16,27 @@ object MongoSeed extends App with ActorInitializer {
   val eventHandler   = system.actorOf(EventHandler.props(), "eventHandler")
   val userHandler    = system.actorOf(UserHandler.props(), "userHandler")
 
-  val Events : ListBuffer[Event] = new ListBuffer[Event]()
+  val events : ListBuffer[Event] = new ListBuffer[Event]()
 
-  Events += EventCreator("Roger Waters", DateTime(2018, 7, 22, 22, 0), "Rock", "The wall", 200, 60.50)
-  Events += EventCreator("Ian Anderson", DateTime(2018, 4, 22, 20, 0), "Rock", "Locomotive Breath", 400, 60.50)
-  Events += EventCreator("Camel", DateTime(2018, 7, 12, 21, 0), "Rock", "Mirage", 250, 70.50)
-  Events += EventCreator("Hans Zimmer", DateTime(2018, 3, 10, 20, 0), "Classic", "Soundtracks", 200, 100)
-  Events += EventCreator("Alborosie", DateTime(2018, 7, 21, 22, 0), "Reggae", "Natural Mystic", 300, 60.50)
-  Events += EventCreator("Manuchao", DateTime(2018, 7, 19, 22, 0), "Reggae", "Clandestino", 200, 30.50)
-  Events += EventCreator("London Philharmonic Orchestra", DateTime(2018, 2, 20, 19, 0), "Classic", "Beethoven Syphony no. 7", 200, 20)
-  Events += EventCreator("Robert Plant", DateTime(2018, 7, 20, 22, 0), "Rock", "Kashmir", 200, 60.50)
+  events += EventCreator("Roger Waters", DateTime(2018, 7, 22, 22, 0), "Rock", "The wall", 200, 60.50)
+  events += EventCreator("Ian Anderson", DateTime(2018, 4, 22, 20, 0), "Rock", "Locomotive Breath", 400, 60.50)
+  events += EventCreator("Camel", DateTime(2018, 7, 12, 21, 0), "Rock", "Mirage", 250, 70.50)
+  events += EventCreator("Hans Zimmer", DateTime(2018, 3, 10, 20, 0), "Classic", "Soundtracks", 200, 100)
+  events += EventCreator("Alborosie", DateTime(2018, 7, 21, 22, 0), "Reggae", "Natural Mystic", 300, 60.50)
+  events += EventCreator("Manuchao", DateTime(2018, 7, 19, 22, 0), "Reggae", "Clandestino", 200, 30.50)
+  events += EventCreator("London Philharmonic Orchestra", DateTime(2018, 2, 20, 19, 0), "Classic", "Beethoven Syphony no. 7", 200, 20)
+  events += EventCreator("Robert Plant", DateTime(2018, 7, 20, 22, 0), "Rock", "Kashmir", 200, 60.50)
 
-  Events.foreach((e : Event) => {
+  events.foreach((e : Event) => {
     eventHandler ? CreateEvent(e)
+  })
+
+  val users : ListBuffer[User] = new ListBuffer[User]()
+
+  users += UserCreator("Alessandro", "123456", "aleskandro@scala")
+  users += UserCreator("Silvia", "1235", "silvia@scala")
+
+  users.foreach((u : User) => {
+    userHandler ? CreateUser(u)
   })
 }
